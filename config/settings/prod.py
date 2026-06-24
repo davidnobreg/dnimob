101ee0f0bec497816905c40265fec00d2b0f594e
David Nóbrega <davidsantos@ads.fiponline.edu.br>
2026-06-24 12:42:28 -0300

    fix: disable SSL redirect, trust NPM proxy headers

--- config/settings/prod.py
+++ config/settings/prod.py
@@ -8,9 +8,11 @@ SECURE_CONTENT_TYPE_NOSNIFF      = True
 SECURE_HSTS_SECONDS               = 31536000
 SECURE_HSTS_INCLUDE_SUBDOMAINS    = True
 SECURE_HSTS_PRELOAD               = True
-SECURE_SSL_REDIRECT               = True
+SECURE_SSL_REDIRECT               = False  # NPM já termina SSL
 SESSION_COOKIE_SECURE             = True
 CSRF_COOKIE_SECURE                = True
+USE_X_FORWARDED_HOST              = True
+SECURE_PROXY_SSL_HEADER           = ('HTTP_X_FORWARDED_PROTO', 'https')
 
 # Arquivos estáticos e mídia via S3
 DEFAULT_FILE_STORAGE    = 'storages.backends.s3boto3.S3Boto3Storage'
